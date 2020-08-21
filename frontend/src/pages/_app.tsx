d760af9e3f43fec33900984a29968bb74ce3ba19
Bruno Dulcetti <brunodulcetti@gmail.com>
2020-08-20 21:03:41 -0300

    feat: next configs and global styles - add initial _app - add initial _document - add initial global styles of project

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,7 +1,19 @@
-import '../styles/globals.css';
+import { AppProps } from 'next/app';
+import Head from 'next/head';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import { GlobalStyles } from 'styles/global';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Papo de Bar</title>
+        <meta name="description" content="Site fodasse pro Papo de Bar" />
+        <link rel="shortcut icon" href="/images/icon-512.png" />
+        <link rel="apple-touch icon" href="/images/icon-512.png" />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
 }
-
-export default MyApp;
