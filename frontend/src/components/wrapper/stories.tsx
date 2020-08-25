1760466e16a13f464200dc486a6edc734f158078
Bruno Dulcetti <brunodulcetti@gmail.com>
2020-08-24 23:59:26 -0300

    chore: remove unecessaries packages

--- frontend/src/components/wrapper/stories.tsx
+++ frontend/src/components/wrapper/stories.tsx
@@ -1,11 +1,9 @@
 import { Story, Meta } from '@storybook/react/types-6-0';
-import { withKnobs, text } from '@storybook/addon-knobs';
 import Wrapper from '.';
 
 export default {
   title: 'Wrapper',
   component: Wrapper,
-  decorators: [withKnobs],
 } as Meta;
 
-export const BasicWrapper: Story = () => <Wrapper title={text('Title', 'Papo de Bar')} />;
+export const BasicWrapper: Story = (args) => <Wrapper {...args} />;
