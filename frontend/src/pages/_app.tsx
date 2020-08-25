ff9612174caaff68f361fbb6f37e31348617813e
Bruno Dulcetti <brunodulcetti@gmail.com>
2020-08-25 00:16:16 -0300

    Add next-pwa initial configs (#7)
    * chore: add next-pwa
    
    * feat: add initial manifest and put on index
    
    * feat: add initial next config

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -11,6 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="description" content="Site fodasse pro Papo de Bar" />
         <link rel="shortcut icon" href="/images/icon-512.png" />
         <link rel="apple-touch icon" href="/images/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
